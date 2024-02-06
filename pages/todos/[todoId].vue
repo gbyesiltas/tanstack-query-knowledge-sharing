6decde6123664af8da71a310552357e49055e864
Gorkem Baris Yesiltas <gorkemyesiltas@hotmail.com>
2024-02-06 16:10:08 +0100

    added to-implement

--- pages/todos/[todoId].vue
+++ pages/todos/[todoId].vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
-import { useQuery } from "@tanstack/vue-query";
-
 const route = useRoute();
 const todoId = route.params.todoId as string;
-
-const { data } = useQuery({
-    queryFn: () => getTodoById(todoId),
-    queryKey: ["todos", todoId],
-});
 
 </script>
 <template>
     <div>
         <h2 class="font-bold text-2xl mb-4">Todo details</h2>
         <div class="rounded-xl bg-gray-700 p-16 w-fit">
-            {{ data?.title }}
+            <!-- to implement-->
         </div>
     </div>
 </template>
