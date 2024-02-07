c1cd8114ab2493aa3630ab1449d57ead2ad7d378
Gorkem Baris Yesiltas <gorkemyesiltas@hotmail.com>
2024-02-07 16:25:29 +0100

    added a text input component

---
+++ components/TextInput.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const text = defineModel<string>({
+    default: "",
+});
+</script>
+
+<template>
+    <input v-model="text" type="text" class="bg-gray-800 rounded-xl p-4 w-96" />
+</template>
--- pages/todos/create.vue
+++ pages/todos/create.vue
@@ -10,7 +10,7 @@ const onSubmit = () => {
     <div>
         <h2 class="text-xl font-bold mb-4">Create a todo item</h2>
         <form @submit="onSubmit" class="flex flex-col">
-            <input v-model="title" type="text" placeholder="Enter your todo" class="bg-gray-800 rounded-xl p-4 w-96" />
+            <TextInput v-model="title" placeholder="Enter your todo" />
             <button type="submit" class="rounded-xl border-white border w-fit p-4 mt-4 bg-gray-800">Add</button>
         </form>
     </div>
