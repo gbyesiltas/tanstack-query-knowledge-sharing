def839322d705372a0dfcf829ffb8d2ee508068d
Gorkem Baris Yesiltas <gorkemyesiltas@hotmail.com>
2024-02-07 17:07:09 +0100

    removed form

--- pages/todos/create.vue
+++ pages/todos/create.vue
@@ -9,9 +9,7 @@ const onSubmit = () => {
 <template>
     <div>
         <h2 class="text-xl font-bold mb-4">Create a todo item</h2>
-        <form @submit="onSubmit" class="flex flex-col">
-            <TextInput v-model="title" placeholder="Enter your todo" />
-            <button type="submit" class="rounded-xl border-white border w-fit p-4 mt-4 bg-gray-800">Add</button>
-        </form>
+        <TextInput v-model="title" placeholder="Enter your todo" />
+        <button @click="onSubmit" class="rounded-xl border-white border w-fit p-4 mt-4 bg-gray-800">Add</button>
     </div>
 </template>
