5a1815f40a501db94ab41ae257d0e5abf7d1b224
Gorkem Baris Yesiltas <gorkemyesiltas@hotmail.com>
2024-02-07 17:08:18 +0100

    added placeholder list

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const todos: any[] = [];
+</script>
+
 <template>
     <div>
         <h2 class="font-bold text-2xl">My todo's</h2>
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                {{ todo.title }}
+            </li>
+        </ul>
     </div>
 </template>
