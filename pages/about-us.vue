a71f50ddd0c6a0ebafe9b9d12d1e03fff18c601f
Gorkem Baris Yesiltas <gorkemyesiltas@hotmail.com>
2024-02-07 15:29:20 +0100

    made the font size the same

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,4 +1,4 @@
 <template>
-    <h2 class="font-bold text-xl">About us</h2>
+    <h2 class="font-bold text-2xl">About us</h2>
     <p>There isn't much that's interesting about us to be honest</p>
 </template>
